ca97d3bc3a6cf2121f70b7ab49a81451edb411f9
jiahua.liu <n@mamoe.net>
2023-09-29 20:40:50 -0400

    to merge

--- contest2/f.cpp
+++ contest2/f.cpp
@@ -1,43 +1,29 @@
 
-
-#include <deque>
 #include <iostream>
-#include <unordered_map>
+#include <deque>
 
 using namespace std;
 
-
-int main(int argc, char* argv[]) {
+int main() {
     int a, b;
     cin >> a;
     cin >> b;
-    //bfs
-    deque<int> q;
-    unordered_map<int, int> visit;
-    q.push_back(a);
-    while (!q.empty()) {
-        int node = q.front();
-        q.pop_front();
-        int new1 = node * 2;
-        int new2 = node * 10 + 1;
-        if (new1 <= b && !visit.count(new1)) {
-            q.push_back(new1);
-            visit[new1] = node;
+    deque<int> path;
+    path.push_front(b);
+    while (b > a) {
+        if (b % 2) {
+            if (b % 10 != 1) {
+                break;
+            }
+            b /= 10;
+        } else {
+            b /= 2;
         }
-        if (new2 <= b && !visit.count(new2)) {
-            q.push_back(new2);
-            visit[new2] = node;
-        }
-        if (new1 == b || new2 == b) break;
-    }
-    if (visit.count(b)) {
-        cout << "YES" << endl;
-        deque<int> path;
         path.push_front(b);
-        for (int i = b; i != a; i = visit[i]) {
-            path.push_front(visit[i]);
-        }
-        cout << path.size() << endl;
+    }
+    if (b == a) {
+        cout << "YES" << endl;
+        cout << (path.size()) << endl;
         while (!path.empty()) {
             cout << path.front() << " ";
             path.pop_front();
@@ -46,7 +32,8 @@ int main(int argc, char* argv[]) {
     } else {
         cout << "NO" << endl;
     }
-
     return 0;
-
 }
+
+
+
