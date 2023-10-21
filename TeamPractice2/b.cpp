1c4120ec32af8f2c10f3903263f567affada904d
AW186 <aw18611578009@gmail.com>
2023-10-20 21:17:41 -0400

    B

--- TeamPractice2/b.cpp
+++ TeamPractice2/b.cpp
@@ -3,8 +3,8 @@
 #define ll short
 
 using namespace std;
-
-vector<vector<vector<int>>> dp;
+//x, y, len, kinks, dir
+vector<vector<vector<vector<vector<char>>>>> dp;
 ll k;
 
 bool possible(ll x, ll y, ll currX, ll currY, const string &s, ll curr, ll rem, const vector<vector<char>> &arr, ll r, ll c)
@@ -18,8 +18,9 @@ bool possible(ll x, ll y, ll currX, ll currY, const string &s, ll curr, ll rem, 
     {
         return rem == 0;
     }
-    if ((dp[x][y][curr] != k + 1) && (dp[x][y][curr] >= rem || (-dp[x][y][curr]) > rem)) {
-        return dp[x][y][curr] > 0;
+    int dirid = (currX + 1) * 3 + (currY+1);
+    if (dp[x][y][curr][rem][dirid]) {
+        return dp[x][y][curr][rem][dirid] > 0;
     }
     vector<pair<ll, ll>> dirs = {
         {1, 0},
@@ -42,12 +43,12 @@ bool possible(ll x, ll y, ll currX, ll currY, const string &s, ll curr, ll rem, 
             if (possible(x + dir.first, y + dir.second, dir.first, dir.second,
                          s, curr + 1, rem + amt, arr, r, c))
             {
-                dp[x][y][curr] = rem;
+                dp[x][y][curr][rem][dirid] = 1;
                 return true;
             }
         }
     }
-    dp[x][y][curr] = -rem - 1;
+    dp[x][y][curr][rem][dirid] = -1;
     return false;
 }
 
@@ -67,7 +68,8 @@ void solve()
 
     string s;
     cin >> k >> s;
-    dp = vector<vector<vector<int>>>(r, vector<vector<int>>(c, vector<int>(s.size(), k + 1)));
+    dp = vector<vector<vector<vector<vector<char>>>>>(r, vector<vector<vector<vector<char>>>>(
+                c, vector<vector<vector<char>>>(s.size(), vector<vector<char>>(k + 1, vector<char>(9)))));
 
     vector<pair<ll, ll>> dirs = {
         {1, 0},
